471269c91e6383b8c871ee45d92c069f0a39b7c6
Kristin <kkaduk@web.de>
2019-02-12 11:18:55 +0100

    first try - added comment

--- bsa_batch_processing.m
+++ bsa_batch_processing.m
@@ -1,3 +1,5 @@
+
+%% Script to load TDT-data
 bsa_read_TDT_data_without_behavior('Y:\Data\TDTtanks\Cornelius_phys\20190201', 'Y:\Projects\PhysiologicalRecording\Data\Cornelius\20190124\bodysignals_without_behavior');
 bsa_read_TDT_data_without_behavior('Y:\Data\TDTtanks\Cornelius_phys\20190121', 'Y:\Projects\PhysiologicalRecording\Data\Cornelius\20190129\bodysignals_without_behavior');
 bsa_read_TDT_data_without_behavior('Y:\Data\TDTtanks\Cornelius_phys\20190201', 'Y:\Projects\PhysiologicalRecording\Data\Cornelius\20190201\bodysignals_without_behavior');
