80ed8d19f419dad568e1d39e39408ec07948b158
lubavas <19555010+lubavas@users.noreply.github.com>
2023-08-08 11:09:58 +0200

    rename: out_ecg --> out; ecg_bna compatible now

--- bsa_ecg_cap_together_analyze_one_session.m
+++ bsa_ecg_cap_together_analyze_one_session.m
@@ -1,4 +1,4 @@
-function [out_ecg,out_cap] = bsa_ecg_cap_together_analyze_one_session(session_path, pathExcel, settings_filename)
+function [out,out_cap] = bsa_ecg_cap_together_analyze_one_session(session_path, pathExcel, settings_filename)
 % bsa_ecg_cap_together_analyze_one_session passes data paths to 
 % bsa_ecg_analyze_one_session and bsa_respiration_analyze_one_session.
 %
@@ -26,42 +26,42 @@ Set.path.cap_save = ...
 Set.path.ecg_cap_save = ...
     [Set.path.ecg_cap_save filesep];
 
-out_ecg = bsa_ecg_analyze_one_session(session_path, pathExcel,settings_filename, Set.path.ecg_save);
+out = bsa_ecg_analyze_one_session(session_path, pathExcel,settings_filename, Set.path.ecg_save);
 
 out_cap = bsa_respiration_analyze_one_session(session_path, pathExcel, settings_filename, Set.path.cap_save);
 
-for blockNum = 1:length(out_ecg)
+for blockNum = 1:length(out)
 
     % preallocate for R peaks in inspiration and expiration
-    is_R_peak_insp = zeros(length(out_ecg(blockNum).Rpeak_sample),1);
-    is_R_peak_exp = zeros(length(out_ecg(blockNum).Rpeak_sample),1);
+    is_R_peak_insp = zeros(length(out(blockNum).Rpeak_sample),1);
+    is_R_peak_exp = zeros(length(out(blockNum).Rpeak_sample),1);
     
-    is_RR_insp = zeros(length(out_ecg(blockNum).R2R_sample),1);
-    is_RR_exp = zeros(length(out_ecg(blockNum).R2R_sample),1);
+    is_RR_insp = zeros(length(out(blockNum).R2R_sample),1);
+    is_RR_exp = zeros(length(out(blockNum).R2R_sample),1);
     
     for currBreath = 1:length(out_cap(blockNum).inspStart_t)
         
         % check if R-peaks belong to a given inspiration / expiration phase
         currInspRpeakIds = ...
-            out_ecg(blockNum).Rpeak_t > out_cap(blockNum).inspStart_t(currBreath) & ...
-            out_ecg(blockNum).Rpeak_t < out_cap(blockNum).inspEnd_t(currBreath);
+            out(blockNum).Rpeak_t > out_cap(blockNum).inspStart_t(currBreath) & ...
+            out(blockNum).Rpeak_t < out_cap(blockNum).inspEnd_t(currBreath);
         currExpRpeakIds = ...
-            out_ecg(blockNum).Rpeak_t > out_cap(blockNum).expStart_t(currBreath) & ...
-            out_ecg(blockNum).Rpeak_t < out_cap(blockNum).expEnd_t(currBreath);
+            out(blockNum).Rpeak_t > out_cap(blockNum).expStart_t(currBreath) & ...
+            out(blockNum).Rpeak_t < out_cap(blockNum).expEnd_t(currBreath);
         
         is_R_peak_insp(currInspRpeakIds) = true;
         is_R_peak_exp(currExpRpeakIds) = true;
         
     end
     
-    if ~isempty(out_ecg(blockNum))
-        out_ecg(blockNum).is_R_peak_insp          = is_R_peak_insp;
-        out_ecg(blockNum).is_R_peak_exp           = is_R_peak_exp;
+    if ~isempty(out(blockNum))
+        out(blockNum).is_R_peak_insp          = is_R_peak_insp;
+        out(blockNum).is_R_peak_exp           = is_R_peak_exp;
     else
-        out_ecg(blockNum).is_R_peak_insp          = [];
-        out_ecg(blockNum).is_R_peak_exp           = [];
+        out(blockNum).is_R_peak_insp          = [];
+        out(blockNum).is_R_peak_exp           = [];
     end
     
-    save([Set.path.ecg_cap_save currDate '_ecg_cap.mat'], 'out_ecg', 'out_cap')
+    save([Set.path.ecg_cap_save currDate '_ecg_cap.mat'], 'out', 'out_cap')
 end
 end
